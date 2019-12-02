1aadc4ffdc4fc563d015b2f89b3c3c05dcd1ebaa
hho114 <hho114@csu.fullerton.edu>
2019-12-02 13:26:20 -0800

    update with camera vision

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-// // Copyright 2018 The Flutter team. All rights reserved.
-// // Use of this source code is governed by a BSD-style license that can be
-// // found in the LICENSE file.
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 // import 'package:flutter/material.dart';
 // import 'package:flutter_ml_vision/ai_use_camera.dart';
@@ -29,10 +29,8 @@
 //   }
 // }
 
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_camera_ml_vision/flutter_camera_ml_vision.dart';
-import 'package:flutter_tts/flutter_tts.dart';
+import 'package:flutter_ml_vision/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,148 +42,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool isTap = false;
-  List<String> data = [];
-  final _scanKey = GlobalKey<CameraMlVisionState>();
-  FlutterTts flutterTts = FlutterTts();
-  String speakText = '';
-  // BarcodeDetector detector = FirebaseVision.instance.barcodeDetector();
-  ImageLabeler detector =
-      FirebaseVision.instance.imageLabeler(ImageLabelerOptions(
-    confidenceThreshold: 0.75,
-  ));
-
-  @override
-  void initState() {
-    super.initState();
-    flutterTts.setSpeechRate(0.8);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: GestureDetector(
-        onTap: () {
-          isTap = true;
-        },
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            CameraMlVision<List<ImageLabel>>(
-              key: _scanKey,
-              detector: detector.processImage,
-              onResult: (labels) {
-                if (isTap) {
-                  for (ImageLabel label in labels) {
-                    if (!mounted) {
-                      return;
-                    } else if (data.contains(label.text)) {
-                      speakText += ' ' + label.text;
-                    } else {
-                      setState(() {
-                        data.add(label.text);
-                        speakText += ' ' + label.text;
-                      });
-                    }
-                  }
-
-                  if (speakText.isEmpty) {
-                    flutterTts.speak('There may be nothing, please try again');
-                  } else {
-                    flutterTts.speak('There $speakText');
-                  }
-
-                  setState(() {
-                    isTap = false;
-                    speakText = '';
-                    // data.clear();
-                  });
-                }
-              },
-              onDispose: () {
-                detector.close();
-              },
-            ),
-            Container(
-              alignment: Alignment.bottomCenter,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ConstrainedBox(
-                    constraints: BoxConstraints(maxHeight: 250),
-                    child: Scrollbar(
-                      child: ListView(
-                        children: data.map((d) {
-                          return Container(
-                            color: Color(0xAAFFFFFF),
-                            child: Padding(
-                              padding: const EdgeInsets.all(16),
-                              child: Text(d),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      // RaisedButton(
-                      //   onPressed: () {
-                      //     _scanKey.currentState.toggle();
-                      //   },
-                      //   child: Text('Start/Pause camera'),
-                      // ),
-                      RaisedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => _SecondScreen()));
-                        },
-                        child: Text('Push new route'),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class _SecondScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: RaisedButton(
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => _SecondScreen(),
-          ));
-        },
-        child: Text('Push new route'),
-      ),
-    );
-  }
-}
